e3ea4d239105cb14e850d5c5eb447a0711412a6d
Montalvas <lucas_montalvani@hotmail.com>
2021-08-10 20:28:29 -0300

    project 5 and 6.

---
+++ project5/decida_por_mim.py
@@ -0,0 +1,19 @@
+from random import choice
+
+class DecidaPorMim:
+    def __init__(self):
+       self.answers = [
+           'Com certeza, você deve fazer isso!',
+           'Não sei, você que sabe.',
+           'Não faz isso não!',
+           'Acho que tá na hora certa!'
+       ]
+    
+    def Iniciar(self):
+        input('Faça sua pergunta: ')
+        print(choice(self.answers))
+
+
+if __name__ == '__main__':
+    decida = DecidaPorMim()
+    decida.Iniciar()
---
+++ project6/decida_por_mim_gui.py
@@ -0,0 +1,35 @@
+from random import choice
+import PySimpleGUI as sg
+
+class DecidaPorMim:
+    def __init__(self):
+       self.answers = [
+           'Com certeza, você deve fazer isso!',
+           'Não sei, você que sabe.',
+           'Não faz isso não!',
+           'Acho que tá na hora certa!'
+       ]
+       
+    def Iniciar(self):
+        self.layout = [
+           [sg.Text('Faça sua pergunta:', size=(40, 0))],
+           [sg.Input()],
+           [sg.Button('Decida por mim')],
+           [sg.Button('Fechar')],
+           [sg.Output(size=(40, 0))]
+       ]
+    
+        self.window = sg.Window('Decida por mim', layout=self.layout)
+        
+        while True:
+            self.event, self.value = self.window.Read()
+        
+            if self.event == 'Decida por mim':
+                print(choice(self.answers))
+            elif self.event == 'Fechar':
+                break
+
+
+if __name__ == '__main__':
+    decida = DecidaPorMim()
+    decida.Iniciar()
